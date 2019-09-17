8c3f9d8c727fa2a538133ed18dfbeb9538ac23f2
atai <1131185063@qq.com>
2019-09-17 11:30:02 +0800

    update

--- web/src/main/java/com/dream/controller/ImageController.java
+++ web/src/main/java/com/dream/controller/ImageController.java
@@ -1,17 +1,21 @@
 package com.dream.controller;
 
 import io.swagger.annotations.ApiOperation;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/image")
 public class ImageController {
 
     @ApiOperation(value = "查询全部照片列表")
@@ -26,5 +30,23 @@ public class ImageController {
             urlList.add(domaimPath+fileName);
         }
         return urlList;
+    }
+
+    @ApiOperation(value = "requestBaidu")
+    @RequestMapping(value = "/requestBaidu",method = RequestMethod.GET)
+    public void requestBaidu(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("https://www.baidu.com").forward(request,response);
+    }
+
+    @ApiOperation(value = "压缩图片")
+    @RequestMapping(value = "/pressImage",method = RequestMethod.GET)
+    public void pressImage() throws Exception {
+        String parentPath = "C:\\Users\\EDZ\\Desktop\\file";
+        String imageName = "123.jpg";
+        File file = new File(parentPath+imageName);
+        Thumbnails.of(parentPath)
+                .scale(0.5f)
+                .outputQuality(0.5f)
+                .toFile(parentPath+"\\newFile");
     }
 }
